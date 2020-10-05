395c3f3b25c892abafdd70dbb59adedfed460aa3
Feilong <xiaomingwang5310feilong@aliyun.com>
2020-10-05 22:51:20 +0800

    4 commit

---
+++ src/main/java/com/feilong/webopen/entity/Customer.java
@@ -0,0 +1,101 @@
+package com.feilong.webopen.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author FeiLong
+ * @version 1.8
+ * @date 2020/10/5 22:47
+ */
+@SuppressWarnings("ALL")
+public class Customer implements Serializable {
+    private int id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String address;
+    private int money;
+    private int state;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String username, String password, String nickname, String address, int money, int state) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.address = address;
+        this.money = money;
+        this.state = state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", address='" + address + '\'' +
+                ", money=" + money +
+                ", state=" + state +
+                '}';
+    }
+}
