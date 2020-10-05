fb6984a859840bcec03766070261a78c58054e2c
Feilong <xiaomingwang5310feilong@aliyun.com>
2020-10-05 22:54:06 +0800

    5 commit

---
+++ src/main/java/com/feilong/webopen/mapper/CustomerMapper.java
@@ -0,0 +1,24 @@
+package com.feilong.webopen.mapper;
+
+import com.feilong.webopen.entity.Customer;
+
+import java.util.List;
+
+/**
+ * @author FeiLong
+ * @version 1.8
+ * @date 2020/10/5 22:51
+ */
+@SuppressWarnings("ALL")
+public interface CustomerMapper {
+
+    void insertCustomer(Customer customer);
+
+    void updateCustomer(Customer customer);
+
+    void deleteCustomerById(int id);
+
+    Customer findCustomerById(int id);
+
+    List<Customer> findAllCustomers();
+}
