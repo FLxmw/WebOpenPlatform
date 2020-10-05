a2f3211959449c1c64cc37215f167fb2b0b200df
Feilong <xiaomingwang5310feilong@aliyun.com>
2020-10-05 23:17:29 +0800

    9 commit

--- src/main/java/com/feilong/webopen/controller/CustomerController.java
+++ src/main/java/com/feilong/webopen/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.feilong.webopen.controller;
 import com.feilong.webopen.entity.Customer;
 import com.feilong.webopen.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping("/addCustomer")
+    @PostMapping("/addCustomer")
     public String addCustomer(Customer customer){
         customerService.insertCustomer(customer);
         return "success";
--- src/main/java/com/feilong/webopen/mapper/AdminUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.feilong.webopen.mapper;
-
-/**
- * @author FeiLong
- * @version 1.8
- * @date 2020/10/5 22:10
- */
-public interface AdminUserMapper {
-}
