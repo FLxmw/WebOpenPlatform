beb265b6c4918c615945cd7875d28878d8a8a14b
Feilong <xiaomingwang5310feilong@aliyun.com>
2020-10-06 17:05:36 +0800

    12 commit

--- src/main/java/com/feilong/webopen/controller/AdminController.java
+++ src/main/java/com/feilong/webopen/controller/AdminController.java
@@ -1,9 +1,17 @@
 package com.feilong.webopen.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author FeiLong
  * @version 1.8
  * @date 2020/10/6 17:01
  */
+@SuppressWarnings("ALL")
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+
 }
